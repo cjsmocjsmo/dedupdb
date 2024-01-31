393037de0ea386e78de9f69e767268bf83166f0d
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-31 04:27:46 -0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::env;
+
+
+pub mod envvars;
+pub mod types;
+
+
+fn main() {
+    envvars::set_env_vars();
+    let db_path = env::var("DUPS_DB");
+    let conn = rusqlite::Connection::open(db_path.unwrap()).unwrap();
+    let mut entryz_list = Vec::new();
+    let mut stmt = conn.prepare("SELECT * FROM dups").unwrap();
+    let entryz = stmt.query_map([], |row| {
+        Ok(types::Meta {
+            imgid: row.get(0)?,
+            imghash: row.get(1)?,
+            imgpath: row.get(2)?,
+        })
+    }).unwrap();
+    for entry in entryz {
+        entryz_list.push(entry.unwrap());
+    }
+
+    for e in entryz_list {
+        println!("{:?}", e);
+    }
+}
